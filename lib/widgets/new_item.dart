c296799c61f76dd4bca00ef42c34fdb36f66ffe8
Bonifacio Conde <b.conde@foodics.com>
2025-04-05 14:04:11 +0800

    Add valiadtors to form fields

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -21,7 +21,13 @@ class _NewItemState extends State<NewItem> {
               TextFormField(
                 maxLength: 50,
                 validator: (value) {
-                  return 'Demo';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters';
+                  }
+                  return null;
                 },
                 decoration: InputDecoration(label: Text('Name')),
               ),
@@ -32,6 +38,15 @@ class _NewItemState extends State<NewItem> {
                     child: TextFormField(
                       initialValue: '1',
                       decoration: InputDecoration(label: Text('Quantity')),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   SizedBox(width: 8),
